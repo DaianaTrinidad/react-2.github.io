d10c9afcf42659622f84a71d01e0f0c0b61d1de0
trinidad <daianatrinidadpuig@gmail.com>
2024-08-17 19:36:04 -0300

    Se cambió la configuracion de react y se volvio a crear la imágen Docker

--- projects/vite-project/vite.config.js
+++ projects/vite-project/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,  // Permite acceso desde todas las interfaces de red
+    port: 3000   // Asegúrate de que el puerto coincida con el del Dockerfile
+  }
+});
